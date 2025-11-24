b1ebe0605ed365f837107040bb62a566c0b2a08f
Parth <parthlakhani112001@gmail.com>
2025-11-24 23:48:49 +0530

    Commit after token

---
+++ auth-service/src/test/java/com/finassist/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finassist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
